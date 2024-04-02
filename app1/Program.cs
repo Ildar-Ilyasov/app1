3135736eda72467d13d496eeadac9ee0d82ab63e
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2024-04-02 17:48:50 +0300

    Добавьте файлы проекта.

---
+++ app1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Programm {
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Введите m");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите n");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int x = m; x <= n; x++)
+        {
+            double num = Math.Pow(x, Math.Sqrt(x));
+            if(num % 1 == 0)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
